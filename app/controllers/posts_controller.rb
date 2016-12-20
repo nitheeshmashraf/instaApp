69c7b444f3c6f34ad0e796d2f0bb85908c89b1a2
Nitheesh M Ashraf <nitheeshmashraf@gmail.com>
2016-12-20 16:08:51 +0400

    link in image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   end
 
   def index
+    @post = Post.all
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image, styles: { medium: "500x500>"}, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
